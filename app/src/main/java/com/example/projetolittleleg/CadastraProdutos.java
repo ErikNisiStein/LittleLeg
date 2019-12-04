0d73ae09e9b522670735aa43a4b702fab091e658
dekopira <andreramosxd@gmail.com>
2019-12-04 00:05:31 -0300

    cadastro funcionando

--- app/src/main/java/com/example/projetolittleleg/CadastraProdutos.java
+++ app/src/main/java/com/example/projetolittleleg/CadastraProdutos.java
@@ -2,8 +2,11 @@ package com.example.projetolittleleg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -13,34 +16,98 @@ import java.util.Date;
 
 public class CadastraProdutos extends AppCompatActivity {
     Date data = new Date();
-    EditText edt_Compra;
+    //campo para receber valor do spin(status)
+    String status;
+
+    //inicializa variaveis
+    EditText dataCompra;
+    EditText dataPag;
+    EditText comprador;
+    EditText valor;
+    EditText produtos;
+
+    //inicializa botão
+    Button inserir;
+    Button limpar;
 
     BD bd;
-
+    DAO banco;
+    Spinner sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastra_produtos);
 
         bd = new BD(this);
 
-        // carega campo dataCompra com a data do SO
-        edt_Compra= findViewById(R.id.edt_Compra);
 
+        //carrega campos
+        dataCompra = findViewById(R.id.edt_DatComp);
+        dataPag = findViewById(R.id.edt_DatPag);
+        comprador = findViewById(R.id.edt_comprador);
+        valor = findViewById(R.id.edt_valor);
+        produtos = findViewById(R.id.edt_Produto);
+        sp = (Spinner) findViewById(R.id.sp_status);
+
+        // carega campo dataCompra com a data do SO
         long date = System.currentTimeMillis();
         SimpleDateFormat sdf = new SimpleDateFormat("d/M/yyyy");
         String dateString = sdf.format(date);
-        edt_Compra.setText(dateString);
+        dataCompra.setText(dateString);
+
+        // carrega botoes
+        inserir = findViewById(R.id.btn_inserir);
+        limpar = findViewById(R.id.btn_limpar);
+
         // carrega spinner
-        String[] arraySpinner = new String[] {
-                " Pendente ", " Pago "
-        };
-        Spinner sp = (Spinner) findViewById(R.id.sp_status);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, arraySpinner);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        sp.setAdapter(adapter);
+        load_spinner(sp);
+
+
+        //função botoes
+        inserir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //verifica status p/ inserção
+                if (sp.equals("Pendente")) {
+                    status = "0";
+                } else if (sp.equals("Pago")) {
+                    status = "1";
+                }
+                //objeto Produto com campos
+                final Produto prod = new Produto(produtos.getText().toString(), dataPag.getText().toString(), dataCompra.getText().toString(),
+                        comprador.getText().toString(), valor.getText().toString(), status);
+                clear();
+
+            }
+        });
+        limpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clear();
+            }
+        });
     }
+
+public void load_spinner(Spinner sp)
+{
+    String[] arraySpinner = new String[] {
+            " Pendente ", " Pago "
+    };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+            android.R.layout.simple_spinner_item, arraySpinner);
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    sp.setAdapter(adapter);
+}
+
+public void clear (){
+        this.comprador.setText("");
+        this.dataPag.setText("");
+        this.valor.setText("");
+        this.produtos.setText("");
+        sp.setSelection(0);
+    }
+
 
 }
